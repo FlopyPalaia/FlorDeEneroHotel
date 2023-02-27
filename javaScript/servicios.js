b977698134b38c9f17c6c65beaf1fd782bcf6aef
Florencia Palaia <87451336+FlopyPalaia@users.noreply.github.com>
2023-02-26 22:30:22 -0300

    Update servicios.js

--- javaScript/servicios.js
+++ javaScript/servicios.js
@@ -8,12 +8,18 @@ contenedor.innerHTML = `<h1>Servicios</h1>
                         <b>Abierto de 7:00 a 23:00</b>
                         <br>
                         <br>
+                        <p>El restaurante tiene mesas adentro y afuera. Durante el invierno la parte externa del restaurante (patio semi cubierto) se cierra para dar lugar a un jardín de invierno en donde los huéspedes pueden disfrutar de sus comidas con vista al jardín, pero sin sufrir el frío.</p>
                         <p>Nuestro hotel cuenta con una huerta en la propiedad, por lo que la gran mayoria de los ingredientes utilizados en la cocina del restaurante son cosechados por nuestros chefs, garantizando su calidad.</p>
                         <p>Servimos platos típicos de la región, además de opciones vegetarianas y celíacas.</p>
                         <br>
                         <h3>Spa</h3>
-                        <b>Abierto de Jueves a Sábado, de 11:00 a 16:00</b>
+                        <b>Abierto de 11:00 a 19:00</b>
                         <br>
                         <br>
-                        <p>Ofrecemos hidroterapia, masajes con aceites esenciales y sauna.</p>`;
+                        <p>Se encuentra en el primer piso y es de uso exclusivo para los huéspedes del hotel. Incluye sauna seco, sauna húmedo, duchas escocesas, pileta climatizada con vista al jardín, masajes con aceites naturales y tratamientos faciales.</p>
+                        <h3>Sala de Reuniones</h3>
+                        <b>Abierto las 24 horas</b>
+                        <br>
+                        <br>
+                        <p>Tiene capacidad para 50 personas, ideal para eventos corporativos y sociales. Consultar precios. Los huéspedes podrán utilizarlo como salón de trabajo cuando no haya ningún evento.</p>`;
 padre.appendChild(contenedor);
